550a836e87f02c084727ccd4a12846b3bd34c3b6
Abhimanyu Singh <98248045+Abhimanyu890@users.noreply.github.com>
2022-09-21 09:46:55 +0530

    Create output\input.cpp

---
+++ Assignment/Assignment 1/output\input.cpp
@@ -0,0 +1,20 @@
+// hacker rank :https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a=1;
+    int b=2;
+    int c=7;
+    cin>>a>>b>>c;
+    int sum;
+    sum=a+b+c;
+    cout<<sum;   
+    return 0;
+}
