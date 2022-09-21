12614bb980a50d310e80a725166848b56a42953b
Abhimanyu Singh <98248045+Abhimanyu890@users.noreply.github.com>
2022-09-21 09:33:11 +0530

    Create hello world.cpp

---
+++ Assignment/Assignment 1/hello world.cpp
@@ -0,0 +1,10 @@
+//hackerrank:https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout<<"Hello, World!";
+    return 0;
+}
