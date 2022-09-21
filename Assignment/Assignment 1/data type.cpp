cae5683ab1a5ee0aaddd42166319ce2ececa2e74
Abhimanyu Singh <98248045+Abhimanyu890@users.noreply.github.com>
2022-09-21 10:00:58 +0530

    Create data type.cpp

---
+++ Assignment/Assignment 1/data type.cpp
@@ -0,0 +1,21 @@
+//hacker rank:https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+   
+    int a;
+    long b;
+    char d;
+    float e;
+    double f;    
+    scanf("%d %ld  %c %f %lf", &a, &b,  &d, &e, &f);
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", d);
+    printf("%f\n", e);
+    printf("%lf", f);
+    return 0;
+}
